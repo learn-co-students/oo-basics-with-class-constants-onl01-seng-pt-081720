fddf84f9e271099c6b758edaa115c144b8d8dcf1
Jazmin Matos <jmatos1993@gmail.com>
2020-09-03 02:13:38 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,22 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = ["Uggs", "Rainbow", "Nike", "Nike"]
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS.uniq! 
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
 
 end
+
+
+# Think about it! When does the BRANDS constant need to be
+    # made aware of the brands of the shoes that are being created?
